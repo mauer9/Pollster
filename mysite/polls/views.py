075307110598a343e983611d44530e96ec5d5d07
mauer <75573495+mauer9@users.noreply.github.com>
2023-11-16 01:01:54 +0600

    templates
    templates for index and detail views of polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("hello, world!")
+    questions = Question.objects.order_by("pub_date")
+    context = {"questions": questions}
+    return render(request, "polls/index.html", context)
 
-def new(request):
-    return HttpResponse("It's NEW page!")
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def result(request, question_id):
+    return HttpResponse(f"You are looking at results of {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"vote for {question_id}")
