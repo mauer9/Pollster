43b3cab0a1590ee06811783b5ba028c396a3989f
mauer <75573495+mauer9@users.noreply.github.com>
2023-11-19 19:50:07 +0600

    voting system
    added vote form for detal view
    after voted redirect to result view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+
+from .models import Choice, Question
 
 
 def index(request):
@@ -12,8 +14,26 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
-def result(request, question_id):
-    return HttpResponse(f"You are looking at results of {question_id}")
-
 def vote(request, question_id):
-    return HttpResponse(f"vote for {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(
+            pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You did not select a choice"
+            }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:result", args=(question.id,)))
+
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/result.html", {"question": question})
