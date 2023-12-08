bf587ad8d9710b0770d93512739ae66db770bb36
mauer <75573495+mauer9@users.noreply.github.com>
2023-12-08 06:14:46 +0600

    login page works

--- pollster/accounts/urls.py
+++ pollster/accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
+    path("logout/", views.logout, name="logout"),
+    path("signup/", views.signup, name="signup"),
 ]
--- pollster/accounts/views.py
+++ pollster/accounts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    context = {'debug_message': request.user}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+        else:
+            context['error_message'] = 'Sorry, your email and password did not match'
+    return render(request, 'accounts/login.html', context)
 
-def register(request):
-    return render(request, 'accounts/register.html')
+def logout(request):
+    auth_logout(request)
+    return redirect('home')
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
