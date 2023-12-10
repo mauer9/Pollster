faff10d565178ecc24465b3207790ce98abdc814
mauer <75573495+mauer9@users.noreply.github.com>
2023-12-11 04:47:53 +0600

    completed signup form

--- pollster/accounts/forms.py
+++ pollster/accounts/forms.py
@@ -1,17 +1,31 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout
+from crispy_forms.layout import Row, Div, HTML, Submit, Layout
 from crispy_bootstrap5.bootstrap5 import FloatingField
 
 class SignupForm(forms.Form):
-    name = forms.CharField(label='your name', max_length=100)
-    email = forms.EmailField(label='your email', max_length=100)
+    first_name = forms.CharField(max_length=100, required=False)
+    last_name = forms.CharField(max_length=100, required=False)
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField(max_length=100, required=False)
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput())
+    confirm_password = forms.CharField(max_length=100, widget=forms.PasswordInput())
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper(self)
-        self.helper.layout = Layout(
-            FloatingField("name", css_class='text-dark'),
-            FloatingField("email", css_class='text-dark'),
-        )
+    helper = FormHelper()
+    helper.layout = Layout(
+        HTML("<h1 class='text-white mb-5'>Sign up to Pollster</h1>"),
+        Row(
+            FloatingField("first_name", wrapper_class='text-dark col-6 pe-1'),
+            FloatingField("last_name", wrapper_class='text-dark col-6 ps-1'),
+        ),
+        FloatingField("username", wrapper_class='text-dark'),
+        FloatingField("email", wrapper_class='text-dark'),
+        Row(
+            FloatingField("password", wrapper_class='text-dark col-6 pe-1'),
+            FloatingField("confirm_password", wrapper_class='text-dark col-6 ps-1'),
+        ),
+        HTML("""<p>Already have an account? Sign In <a href="{% url 'accounts:login' %}" class="link-light link-offset-2">Log In</a></p>"""),
+        Submit("submit", "Submit", css_class="btn btn-primary"),
+    )
+    helper.render_required_fields = True
 
