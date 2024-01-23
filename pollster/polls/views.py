83a1613a571a727e4360ed4ec5a0f994758d40cb
mauer <75573495+mauer9@users.noreply.github.com>
2024-01-24 01:41:40 +0600

    voted poll ui
    render selected choices if voted

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -13,26 +13,41 @@ class IndexView(generic.ListView):
     context_object_name = "polls"
 
     def get_queryset(self):
-        queryset = Poll.objects.filter(pub_date__lte=timezone.now(), choice__isnull=False)
+        queryset = Poll.objects.filter(
+            pub_date__lte=timezone.now(), choice__isnull=False
+        )
         queryset = queryset.distinct()
-        queryset = queryset.order_by("-pub_date")
-        return queryset
+        return queryset.order_by("-pub_date")
+
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = "polls/detail.html"
 
     def get_queryset(self):
-        queryset = Poll.objects.filter(pub_date__lte=timezone.now(), choice__isnull=False)
-        queryset = queryset.distinct()
-        return queryset
+        queryset = Poll.objects.filter(
+            pub_date__lte=timezone.now(), choice__isnull=False
+        )
+        return queryset.distinct()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         poll = self.get_object()
         choices = poll.get_choices_with_params()
-        context['choices'] = choices
+        context["choices"] = choices
+
+        if not self.request.user.is_authenticated:
+            return context
+
+        # if user already voted in the current pole
+        # pass the choice to template so voted choice will be checked
+        all_user_votes = Vote.objects.filter(user=self.request.user)
+        vote = all_user_votes.filter(poll=poll).first()
+        if vote:
+            context["user_choice"] = vote.choice
+
         return context
+
 
 @login_required(redirect_field_name=None)
 def vote(request, pk):
