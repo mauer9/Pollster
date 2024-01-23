4fbc2dc0f4d2d3e7981cdc693875cc4b4c880a50
mauer <75573495+mauer9@users.noreply.github.com>
2024-01-24 00:07:49 +0600

    minor styling changes

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -6,15 +6,12 @@ from django.utils import timezone
 from django.contrib import admin
 from django.contrib.auth.models import User
 
+
 class Poll(models.Model):
     text = models.CharField(max_length=200)
-    pub_date = models.DateField('date published')
+    pub_date = models.DateField("date published")
 
-    @admin.display(
-        boolean = True,
-        ordering='pub_date',
-        description='Published recently?'
-    )
+    @admin.display(boolean=True, ordering="pub_date", description="Published recently?")
     def recently_published(self):
         now = timezone.now().date()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
@@ -26,31 +23,44 @@ class Poll(models.Model):
     def get_choices_with_params(self):
         res = []
         choices = self.choice_set.all()
-        choices = choices.annotate(vote_count=Count('vote'))
+        choices = choices.annotate(vote_count=Count("vote"))
         choices = choices.order_by("-vote_count")
-        colors = cycle(['info', 'danger', 'success', 'secondary',])
-            
+        colors = cycle(
+            [
+                "info",
+                "danger",
+                "success",
+                "secondary",
+            ]
+        )
+
         for choice in choices:
             d = {
-                'id': choice.id,
-                'text': choice.text,
-                'votes': choice.vote_count,
-                'color': next(colors),
+                "id": choice.id,
+                "text": choice.text,
+                "votes": choice.vote_count,
+                "color": next(colors),
             }
 
-            d['percent'] = (d['votes'] / self.get_total_votes) * 100 if d['votes'] else 0
+            d["percent"] = (
+                (d["votes"] / self.get_total_votes) * 100 if d["votes"] else 0
+            )
 
             res.append(d)
 
         return res
 
-    def __str__(self): return self.text
+    def __str__(self):
+        return self.text
+
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
 
-    def __str__(self): return self.text
+    def __str__(self):
+        return self.text
+
 
 class Vote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -58,4 +68,4 @@ class Vote(models.Model):
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.user} - {self.poll} - {self.choice}'
+        return f"{self.user}:{self.choice}:{self.poll}"
--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 
-app_name = 'polls'
+app_name = "polls"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/vote", views.vote, name="vote"),
+    path("<int:pk>/vote/", views.vote, name="vote"),
 ]
